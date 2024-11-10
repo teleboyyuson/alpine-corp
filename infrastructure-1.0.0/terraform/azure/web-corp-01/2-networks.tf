8c9c63e92aab710daad6a71cb6a4fca59fdf2755
teleboyyuson <teleboy.yuson@gmail.com>
2024-11-11 00:26:43 +0800

    update

--- infrastructure-1.0.0/terraform/azure/web-corp-01/2-networks.tf
+++ infrastructure-1.0.0/terraform/azure/web-corp-01/2-networks.tf
@@ -4,12 +4,16 @@ resource "azurerm_virtual_network" "vn" {
   location            = "${var.location}"
   resource_group_name = "rg-${terraform.workspace}-${var.rg_name}"
   address_space       = "${var.cidr_blocks}"
+
+  depends_on = [azurerm_resource_group.rg]
 }
 
 # Subnet for VMs
-resource "azurerm_subnet" "sb" {
-  name                 = "sb-${terraform.workspace}-${var.rg_name}"
+resource "azurerm_subnet" "sn" {
+  name                 = "sn-${terraform.workspace}-${var.rg_name}"
   resource_group_name  = "rg-${terraform.workspace}-${var.rg_name}"
   virtual_network_name = "vn-${terraform.workspace}-${var.rg_name}"
   address_prefixes     = "${var.subnet}"
+
+  depends_on = [azurerm_virtual_network.vn]
 }
