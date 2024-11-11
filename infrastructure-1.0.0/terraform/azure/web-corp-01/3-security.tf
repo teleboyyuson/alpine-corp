66c3fe1220f340a75552510d0697bd04f5fcfa5e
teleboyyuson <teleboy.yuson@gmail.com>
2024-11-11 09:12:29 +0800

    updates on nsgs

--- infrastructure-1.0.0/terraform/azure/web-corp-01/3-security.tf
+++ infrastructure-1.0.0/terraform/azure/web-corp-01/3-security.tf
@@ -25,6 +25,8 @@ resource "azurerm_network_security_group" "nsg_lb" {
     destination_port_range     = 443
     source_address_prefix      = "*"
     destination_address_prefix = "*"
+
+    depends_on = [azurerm_resource_group.rg]
   }
 }
 
@@ -55,5 +57,7 @@ resource "azurerm_network_security_group" "nsg_web" {
     destination_port_range     = 22
     source_address_prefix      = "*"
     destination_address_prefix = "*"
+
+    depends_on = [azurerm_resource_group.rg]
   }
 }
