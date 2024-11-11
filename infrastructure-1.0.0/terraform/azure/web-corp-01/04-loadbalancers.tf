7cd4a048ba900e211ad723b757c8d7b2234266a8
teleboyyuson <teleboy.yuson@gmail.com>
2024-11-11 09:45:55 +0800

    lb updates

--- infrastructure-1.0.0/terraform/azure/web-corp-01/04-loadbalancers.tf
+++ infrastructure-1.0.0/terraform/azure/web-corp-01/04-loadbalancers.tf
@@ -4,7 +4,7 @@ resource "azurerm_public_ip" "lb_pip" {
   resource_group_name = "rg-${terraform.workspace}-${var.rg_name}"
   location            = "${var.location}"
   allocation_method   = "Static"
-  sku                  = "Basic"
+  sku                  = "Standard"
 
   depends_on = [azurerm_resource_group.rg]
 }
@@ -14,7 +14,7 @@ resource "azurerm_lb" "lb_web" {
   name                = "lb_web-${terraform.workspace}-${var.rg_name}"
   resource_group_name = "rg-${terraform.workspace}-${var.rg_name}"
   location            = "${var.location}"
-  sku                 = "Basic"
+  sku                 = "Standard"
 
   frontend_ip_configuration {
     name                 = "lb_fip-${terraform.workspace}-${var.rg_name}"
